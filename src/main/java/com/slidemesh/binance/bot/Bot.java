685d32e099e6b551352c08bda74dfa8f1b93c2c1
浩 <inf2inf2@outlook.com>
2021-11-13 16:22:21 +0800

    add bot interface

--- src/main/java/com/slidemesh/binance/bot/Bot.java
+++ src/main/java/com/slidemesh/binance/bot/Bot.java
@@ -2,4 +2,30 @@ package com.slidemesh.binance.bot;
 
 public interface Bot {
 
+    /**
+     * 机器人名称
+     */
+    void name();
+
+    /**
+     * 机器人交易的对
+     * @return Symbol
+     */
+    Symbol symbol();
+
+    /**
+     * 启动机器人
+     */
+    void run();
+
+    /**
+     * 停止机器人
+     */
+    void stop();
+
+    /**
+     * 当价格更新
+     */
+    void onPriceUpdate(Object data);
+
 }
