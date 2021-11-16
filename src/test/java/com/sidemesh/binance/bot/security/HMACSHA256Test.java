c019061327896048750c678029c570338925fafd
浩 <inf2inf2@outlook.com>
2021-11-16 15:33:31 +0800

    hmacsha256

---
+++ src/test/java/com/sidemesh/binance/bot/security/HMACSHA256Test.java
@@ -0,0 +1,18 @@
+package com.sidemesh.binance.bot.security;
+
+import com.slidemesh.binance.bot.security.HMACSHA256;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class HMACSHA256Test {
+
+    @Test
+    public void testSign() {
+        String message = "hello world!";
+        String secret = "pass";
+
+        assertNotNull(HMACSHA256.sign(message, secret));
+    }
+
+}
